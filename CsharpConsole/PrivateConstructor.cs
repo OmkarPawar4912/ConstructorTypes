f4f37cbf3796596a7c90fe956ddc1cef1ba61465
dev-Omkar <opawar88@gmail.com>
2021-12-06 14:35:39 +0530

    After Seen Feedback

---
+++ CsharpConsole/PrivateConstructor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpConsole
+{
+    class PrivateConstructor
+    {
+        private PrivateConstructor() // Private Constructor
+        {
+
+        }
+        public static string GetCalled() // Display Private Constructor Method Called
+        {
+            return "Private Constructor => Private Constructor Called";
+        }
+    }
+}
--- CsharpConsole/Program.cs
+++ CsharpConsole/Program.cs
@@ -7,58 +7,6 @@ public enum Season
     Summer,
     Winter
 }
-public class ConstrTypes
-{
-    public int no1, no2;
-    private string name;
-    private int age;
-    public static float _PI;
-    
-    static ConstrTypes() // Static constructor
-    {
-        _PI = 3.142F;
-    }
-   
-    public ConstrTypes()
-    {
-        no1 = 100;
-        no2 = 200;
-        Console.WriteLine("Default Contructor =>  No 1 : {0}   No 2 : {1} ", no1,no2);
-    }
-    public ConstrTypes(int no3, int no4) //Parameterized constructor
-    {
-        this.no1 = no3;
-        this.no2 = no4;
-    }
-    public ConstrTypes(string name, int age)  // Instance constructor.
-    {
-        this.name = name;
-        this.age = age;
-    }
-    public ConstrTypes(ConstrTypes CopyConstr)   // Declar Copy constructor.
-    {
-        name = CopyConstr.name; ;
-        age = CopyConstr.age;
-    }
-    public string GetDetiles     //Get Copy Details
-    {
-        get
-        {
-            return "Copy Contructor =>  Name : " + name + "   Age : "+ age.ToString();
-        }
-    }
-}
-class PrivateConstructor
-{
-    private PrivateConstructor() // Private Constructor
-    {
-
-    }
-    public static string GetCalled() // Display Private Constructor Method Called
-    {
-        return "Private Constructor => Private Constructor Called";
-    }
-}
 public class Program
 {
 public static void Main()
